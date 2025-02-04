2fd801d74eae56260b4a9e5d4304dbceaf847d62
harrison curtis <harrisoncurtis50@gmail.com>
2025-02-04 17:55:08 +0000

    test.py running but needs further fucniton check and more models from the paper built

--- test.py
+++ test.py
@@ -1,7 +1,9 @@
 from simulations import simulate
-from cmdstanpy import CmdStanModel
+from cmdstanpy import CmdStanModel, write_stan_json
+import arviz as az
+
 # Complie stan model
-model = CmdStanModel(stan_file=".\Stan\fixed_nc.stan")
+model = CmdStanModel(stan_file="Stan/fixed_nc.stan")
 
 # Example usage:
 # Ensure p, q, and s are defined
@@ -19,3 +21,24 @@ sub_num = data['sub_num'].values
 u0_stim_data = data.iloc[:,:(int(q//2))].sum(axis=1).values
 u1_stim_data = data.iloc[:,(int(q)//2):].sum(axis=1).values
 stim_data = data.iloc[:, :int(q)]
+print(data.columns)
+
+stan_data = {
+    'N': len(data['y']),
+    'n_stims': q,
+    'n_subs': data['sub_num'].nunique(),
+    'sub_num': data['sub_num'].values + 1,
+    'u0_stim_data': u0_stim_data,
+    'u1_stim_data': u1_stim_data,
+    'stim_data': stim_data,
+    'y': data['y'].values,
+    'y_t_1': data['y_t-1'].values,
+    'y_t_2': data['y_t-2'].values,
+    'ar_flag': 1
+    }
+
+write_stan_json("data.json", data = stan_data)
+
+fit = model.sample("data.json", chains = 4 , iter_sampling=500, parallel_chains = 4)
+# TODO: No warnins given write model check code.
+print(az.summary(fit))
